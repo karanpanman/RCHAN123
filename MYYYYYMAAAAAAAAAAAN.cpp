3323a70290d1188fc57c0575ceed28e5e338e011
karanpanman <kbhog001@ucr.edu>
2021-05-15 17:12:57 -0700

    FIRST COMMIT

---
+++ MYYYYYMAAAAAAAAAAAN.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+
+string first = "RYAN CHANG";
+string check;
+
+cout << "LET'S GOOOOOOO KING HAAAAAAAAAAAAAAAPPY GOD DANG CUCKING BIRTHDAY \n\n\n";
+
+cout << "ALRIGHT GET READY FOR THIS CHIEF\n" << endl;
+
+string t1;
+//question 1
+cout << "WHO IS THE SUPERIOR CHANG?????" << endl;
+while ( t1 != "RYAN CHANG" ) {
+	getline(cin, t1);
+	if ( t1 != first ) { cout << "NO! TRY AGAIN!\n\n\nWHO IS THE BEST CHANG?????\n"; }
+
+}
+
+cout << "\nALRIGHT ALRIGHT I SEE YOU, KEEP THAT SAME ENERGY" << endl;
+
+cout << "\n";
+//question 2
+first = "OUI";
+cout << "WHAT IS THE ON GOD NATIONAL ANTHEM?!?!?!" << endl;
+while ( t1 != "OUI" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "DUMMY DOG NO!\n\n\nWHAT IS THE ON GOD NATIONAL ANTHEM MY GUY?????\n"; }
+
+}
+
+cout << "\nSHEEEEEEEEEEEEEEEEEESH" << endl;
+
+cout << "\n";
+
+
+//question 3
+cout << "GREATEST CHESS MOVE OF ALL TIME?!?!?!" << endl;
+while ( t1 != "RUY LOPEZ DECLINED ACCEPTED" ) {
+        getline(cin, t1);
+        if ( t1 != "RUY LOPEZ DECLINED ACCEPTED" ) { cout << "HOW DO YOU GET HIS WRONG!\n\n\nWHAT IS THE BEST CHESS MOVE EVER?????\n";  
+	}
+}
+
+cout << "THIS NEXT ONE IS A REAL DOOZY: " << endl << endl;
+
+//question 4
+cout << "FIRST CHINESE PHRASE YOU TAUGHT US IN CHINESE LECTURE?!?!?!" << endl;
+while ( t1 != "BOOSHI JUAN" ) {
+        getline(cin, t1);
+        if ( t1 != "BOOSHI JUAN" ) { cout << "RHYMES WITH POOPY SWAN!\n\n\nTHE GREATEST CHINESE PHRASE OF ALL TIME?????\n";                 
+        }
+}
+
+cout << "LET'S GOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << endl << endl;
+
+//question 5
+cout << "ROAD TO ____" << endl;
+while ( t1 != "GOLD" ) {
+        getline(cin, t1);
+        if ( t1 != "GOLD" ) { cout << "TRY AGAIN!!\n\n\nROAD TO?????\n";
+        }
+}
+
+cout << "THAT'S POPPINGGGGG" << endl << endl;
+
+//question 6
+cout << "THE BEST CREAMER OF ALL TIME?!?!?!" << endl;
+while ( t1 != "HAZELNUT" ) {
+        getline(cin, t1);
+      if ( t1 != "HAZELNUT" ) { cout << "UCR MAN!!!\n\n\nTHE BEST CREAMER EVERRR?????\n";
+        }
+}
+
+cout << "YOU ALREADY KNOWWWWWW" << endl << endl;
+
+//question 7
+first = "PLAY PIANO";
+cout << "WHAT DID YOU DO WHILE GROWING UP?!?!?!" << endl;
+while ( t1 != "PLAY PIANO" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "buddY PLEASEEEEE!!!\n\n\nWHAT DID YOU DO GROWING UP?????\n";
+    	}
+}
+
+cout << "that's FACTS MY GGGGGGGGGG" << endl << endl;
+
+return -1;
+}
