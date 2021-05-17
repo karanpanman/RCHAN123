d10efc891c22c826e01dc4968a6b8be2462a83df
karanpanman <74081077+karanpanman@users.noreply.github.com>
2021-05-16 22:33:12 -0700

    Update MYYYYYMAAAAAAAAAAAN.cpp

--- MYYYYYMAAAAAAAAAAAN.cpp
+++ MYYYYYMAAAAAAAAAAAN.cpp
@@ -127,7 +127,7 @@ cout << "CRUZ IS PROUD OF YOU" << endl << endl;
 	
 //question 10
 first = "STUSSY";
-cout << "YOU AND DYLAN FLAKED PHYSICS TO GO GET WHAT BRAND CREWNECK?" << endl;
+cout << "YOU AND DYLAN FLAKED PHYSICS TO GO GET WHAT BRAND CREW NECK?" << endl;
 while ( t1 != "STUSSY" ) {
         getline(cin, t1);
         if ( t1 != first ) { cout << "DOG DOG DOG\n\n\nNIBBA LOOKING FRESH IN THAT PINK PINK?\n";
@@ -153,14 +153,104 @@ first = "HAND";
 cout << "WHAT BODY PART OF CRUZ'S DO YOU GET 5 MINUTES ON??" << endl;
 while ( t1 != "HAND" ) {
         getline(cin, t1);
-        if ( t1 != first ) { cout << "WTF YOU'RE A FREAK, TRY AGAIN JESUS CHRIST\n\n\n 5 MINUTES ON THE ?????\n";
+        if ( t1 != first ) { cout << "WTF YOU'RE A FREAK, TRY AGAIN JESUS CHRIST\n\n\n5 MINUTES ON THE ?????\n";
     	}
 }
 
 cout << "CRUZ IS ACTUALLY NOT SO PROUD OF THIS ONE" << endl << endl;
 	
 
+//question 10
+first = "JEFFREY";
+cout << "THE PERSON THAT WILL HARRASS AND FONDLE CRUZ WITH YOU THIS FALL QUARTER" << endl;
+while ( t1 != "JEFFREY" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "I'M SAD YOU HAVE MULTIPLE PEOPLE IN MIND FOR THIS\n\n\nFONDLE CRUZ WITH WHO??\n";
+    	}
+}
 
+cout << "I'M ACTUALLY VERY WORRIED, PLEASE DON'T HURT JOSEPH AND CRUZ" << endl << endl;
+
+
+//question 10
+first = "LORENZO";
+cout << "THIS GUY LITERALLY SENT YOU A PICTURE OF HIS POOP IN A TOILET" << endl;
+while ( t1 != "JEFFREY" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "YOU'RE ACTUALLY DISGUSTING, HOW MANY PEOPLE SEND YOU POOP PHOTOS?\n\n\nPOOP??\n";
+    	}
+}
+
+cout << "YOU'RE ONE SICK BASTARD ASKING FOR THAT PHOTO LMFAOOOOOOO" << endl << endl;
+	
+	
+first = "CUCK EM";
+cout << "FIRST RULE OF LEEG AND LIFE" << endl;
+while ( t1 != "CUCK EM" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "DON'T SUCK EM, INSTEAD ......\n\n\nFIRST RULE OF LIFE??\n";
+    	}
+}
+
+cout << "I'M GODDANG PROUD OF YA" << endl << endl;
+	
+	
+first = "TRASH COMPACTORS";
+cout << "Nic knows the coding and processors, you know the ............." << endl;
+while ( t1 != "TRASH COMPACTORS" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "THIS RHYMES WITH FLASH TRACTORS\n\n\nNIBBA CHILI DOG\n";
+    	}
+}
+
+cout << "OKAY OKAY I FEEL YOU KEEP GOING DAWG" << endl << endl;
+	
+
+first = "MAYBE";
+cout << "IF YOU WERE TO ASK SANJEM A YES OR NO QUESTION, HIS RESPONSE IS?" << endl;
+while ( t1 != "MAYBE" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "HEEEEEEEEYYYYYYYYY\n\n\nYES OR NO BUT SANJEM STYLE\n";
+    	}
+}
+
+cout << "HEEEEEEEEEEEEEY SANJEM IS IMPRESSED YOU KNOW HIM SO WELL" << endl << endl;
+	
+	
+first = "CAMERON";
+cout << "WHO NEEDS MORE SLEEP?" << endl;
+while ( t1 != "CAMERON" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "SCHLEPPY SCHLEEP \n\n\nTHIS MAN NEEDS SOME SCHLEEP NO CAP\n";
+    	}
+}
+
+cout << "HEEEEEEEEEEEEEY SANJEM IS IMPRESSED YOU KNOW HIM SO WELL" << endl << endl;
+
+	
+first = "NANCY";
+cout << "BEST UCR PROFESSOR, SHE TAUGHT THE MOST HELPFUL CLASS OF ALL TIME" << endl;
+while ( t1 != "NANCY" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "BRUH WE ON A FIRST NAME BASIS W HER\n\n\nBEST PROFESSOR AT UCR OBJECTIVELY\n";
+    	}
+}
+
+cout << "THAT'S ON GODDDDDDDDDDD!!!!!!!!!!!" << endl << endl;
+	
+	
+first = "ON GOD";
+cout << "THE GREATEST DISCORD SERVER OF ALL TIME??" << endl;
+while ( t1 != "ON GOD" ) {
+        getline(cin, t1);
+        if ( t1 != first ) { cout << "CUCK WHATEVER THE CUCK KINDA OTHER SERVER YOU JUST SAID\n\n\nWE AIN'T ON JAH, BUT WE.......\n";
+    	}
+}
+
+cout << "THAT'S ON GODDDDDDDDDDD!!!!!!!!!!!" << endl << endl;
+	
+
+	
 
 cout << "OKAY LAST ONE HOMIE DAWG" << endl << endl;
 	
@@ -176,6 +266,10 @@ while ( t1 != "PHYSICS" ) {
 
 cout << "CONGRATULTIONS!!! HAPPY BIRTHDAY G! ALTHOUGH YOU'RE GREMLIN MATERIAL, WE CUCKING LOVE YA DAWG" << endl << endl;
 	
+	
+cout << "HERE IS YOUR CUCKING GIFT: PLAYSTATION PLUS CARD!!!!! YOU NOW HAVE 1 YEAR OF PS PLUS, GO GET ALL YOUR FREE GAMES, DOWNLOAD BATTLEFIELD V AND PLAY W US, AND STAY FAKE G!";
+
+cout << endl << endl;
 
 	
 
